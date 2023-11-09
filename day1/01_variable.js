437cf42e856f9f37ef13354206b1acee5c30b368
Bhavya Kashmira <103055855+bhavyakashmira@users.noreply.github.com>
2023-11-09 14:29:42 +0000

    day 1 and day2

---
+++ day1/01_variable.js
@@ -0,0 +1,7 @@
+const name = 'bhavya'
+let val =10
+var age = 15
+
+/* var are not used as they have issue with code block scope */
+
+console.table([name , val , age]);
---
+++ day1/02_data.js
@@ -0,0 +1,16 @@
+"use strict " // treats all JS code as newer version
+let name = " string"
+let age = 3
+let islogin = true
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+console.log(typeof name)
+console.log(typeof undefined); // undefined
+console.log(typeof null); // objec
