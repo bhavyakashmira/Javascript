51b27abc5760d90c8106ab2b452af068d9389c72
Bhavya Kashmira <103055855+bhavyakashmira@users.noreply.github.com>
2023-11-10 06:01:22 +0000

    functions

---
+++ module3/01_functions.js
@@ -0,0 +1,12 @@
+// function addition(num1, num2){
+//     console.log(num1+num2)
+// }
+// addition(1,2)
+
+function calculate(...num1){
+    return num1
+}
+console.log(calculate(200,400,500))
+
+//functions with objects
+
