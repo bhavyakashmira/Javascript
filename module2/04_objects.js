051b5740ca33be34173ec1e25cfb0dad920b551e
Bhavya Kashmira <103055855+bhavyakashmira@users.noreply.github.com>
2023-11-10 05:19:20 +0000

    json and api

---
+++ module2/04_objects.js
@@ -0,0 +1,47 @@
+// const user = new Object()
+
+const user = {}
+user.id = "1234"
+user.name ="bhavya"
+user.islogin = false;
+
+const regular  ={
+    email : "some@gmail.com",
+    fullname :{
+        userfullname:{
+        firstname : 'bhavya',
+        lastname : 'kashmira'
+        }
+    }
+}
+console.log(regular.fullname)
+
+console.log(user)
+
+const course = {
+    coursename : "js",
+    price : "99",
+    courseteach :'bhavya'
+}
+
+const {courseteach:teach}= course
+console.log(teach)
+
+// const navbar=({company})=>{
+ 
+// }
+// navbar(company ="delhi")
+
+// {
+//  name:" bhavya",
+//  course : "js" ,
+//  price :"free"
+
+// }
+
+[
+    {},
+    {},
+    {}
+]
+
