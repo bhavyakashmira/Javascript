739044522bf5329811f903aea6e2fd50a2955560
Bhavya Kashmira <103055855+bhavyakashmira@users.noreply.github.com>
2023-11-10 06:14:19 +0000

    scope declaration

---
+++ module3/02_scope.js
@@ -0,0 +1,35 @@
+// let a = 300
+
+// if(true){
+//    let a =30
+//    console.log(a)
+// }
+// console.log(a)
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+//  one() 
+
+//variable way of declaring the function and we cant access them before declaring unlike normal function declaration 
+
+//addtwo  
+const addtwo = function(num){
+    return num
+    
+    
+}
+
+console.log(addtwo(3))
+
+
