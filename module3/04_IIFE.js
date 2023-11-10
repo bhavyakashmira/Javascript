f300961364a5b426210feea98e101645a263d857
Bhavya Kashmira <103055855+bhavyakashmira@users.noreply.github.com>
2023-11-10 06:26:58 +0000

    iife

---
+++ module3/04_IIFE.js
@@ -0,0 +1,15 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+// function chai(){
+//     console.log("DB connected")
+// }
+// chai()
+
+// iife way
+// (function chai(){
+//     console.log("DB connected");
+// })();
+
+(()=>{
+    console.log("DB connected")
+})();
