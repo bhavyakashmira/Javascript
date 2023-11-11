307e983c0f619e3cc3292332fd69d96c91b2e1e8
Bhavya Kashmira <103055855+bhavyakashmira@users.noreply.github.com>
2023-11-11 04:25:41 +0000

    switch cases

---
+++ module4/02_swtich.js
@@ -0,0 +1,28 @@
+// // switch (key) {
+// //     case value:
+        
+// //         break;
+
+// //     default:
+// //         break;
+// // }
+
+// const month =3
+
+// switch (month) {
+//     case value:
+//         console.log(10)
+//         break;
+//     case value:
+//         console.log(10)
+//         break;
+//     case value:
+//         console.log(10)
+//         break;
+//     case value:
+//         console.log(10)
+//         break;
+
+//     default:
+//         break;
+// }
