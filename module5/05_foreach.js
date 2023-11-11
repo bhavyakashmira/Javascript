217a8e18ac2aaad70c631e39cb911504c92ce8a8
Bhavya Kashmira <103055855+bhavyakashmira@users.noreply.github.com>
2023-11-11 04:59:12 +0000

    for each

---
+++ module5/05_foreach.js
@@ -0,0 +1,25 @@
+// const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// coding.forEach((item)=>{
+//     console.log(item);
+// })
+
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+]
+
+myCoding.forEach((item)=>{
+    console.log(item.languageFileName);
+})
